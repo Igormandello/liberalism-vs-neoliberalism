75f1428ea49724533bd877f0e7629c0a7e71b04b
Igor Mandello <igormandello@gmail.com>
2018-11-16 03:34:30 -0200

    Implements the introduction section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ class App extends Component {
     return (
       <div className="app">
         <Header />
+        <section className="intro">
+          <h2>Introdução</h2>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but 
+            the majority have suffered alteration in some form, by injected humour, 
+            or randomised words which don't look even slightly believable.
+          </p>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but 
+            the majority have suffered alteration in some form, by injected humour, 
+            or randomised words which don't look even slightly believable.
+          </p>
+          <cite>Os Autores</cite>
+        </section>
       </div>
     );
   }
