c9484d67adaa6434b892db9bd85fa7ad6cab61bc
Igor Mandello <igormandello@gmail.com>
2018-11-16 04:45:04 -0200

    Creates a TimeLineData component
    - Adds a Consts file
    
    - Improves some TimeLine designs

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TimeLineTitle from './TimeLineTitle';
+import TimeLineData from './TimeLineData';
 import '../css/TimeLine.css';
 
 function TimeLine() {
   return (
     <div className="timeLine">
       <TimeLineTitle text="Surgimento"/>
+      <TimeLineData />
       <TimeLineTitle text="Utilização"/>
       <TimeLineTitle text="Crise"/>
     </div>
---
+++ src/components/TimeLineData.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../css/TimeLineData.css';
+
+function TimeLineData(props) {
+  return (
+    <div className="timeLineData">
+      <div className="titles">
+        <span>Liberalismo</span>
+        <span>Neoliberalismo</span>
+      </div>
+      <div className="data">
+        <div>
+          <h3>1860...</h3>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form, by injected 
+            humour, or randomised words which don't look even slightly believable. 
+            If you are going to use a passage of Lorem Ipsum, you need to be 
+            sure there isn't anything embarrassing hidden in the middle of text.
+          </p>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form, by injected 
+            humour, or randomised words which don't look even slightly believable. 
+            If you are going to use a passage of Lorem Ipsum, you need to be 
+            sure there isn't anything embarrassing hidden in the middle of text.
+          </p>
+        </div>
+        <div>
+          <h3>2000...</h3>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form, by injected 
+            humour, or randomised words which don't look even slightly believable. 
+            If you are going to use a passage of Lorem Ipsum, you need to be 
+            sure there isn't anything embarrassing hidden in the middle of text.
+          </p>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, 
+            but the majority have suffered alteration in some form, by injected 
+            humour, or randomised words which don't look even slightly believable. 
+            If you are going to use a passage of Lorem Ipsum, you need to be 
+            sure there isn't anything embarrassing hidden in the middle of text.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TimeLineData;
