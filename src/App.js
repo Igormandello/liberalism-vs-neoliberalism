997c6c2304edf01d8fb873f7e13e36f51e23df7b
Igor Mandello <igormandello@gmail.com>
2018-11-16 01:41:51 -0200

    Structures the Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div className="main">
+        <h1>
+          Liberalismo e Neoliberalismo
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, 
+          recusandae quisquam quasi amet inventore, fugiat explicabo animi error 
+          itaque quam architecto repellat consequatur, doloremque veniam beatae 
+          autem unde nulla in?
+        </p>
+      </div>
+      <div className="authors">
+        <p>Autores</p>
+        <span>Gabriel Pallotta</span>
+        <span>Igor Mandello</span>
+        <span>Vitor Bisterso</span>
+        <span>Vitor Bartier</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
